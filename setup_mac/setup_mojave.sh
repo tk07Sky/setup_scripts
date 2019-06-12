92b1434f7708a5b9368e8d1bc77b9bca4c5dd0db
Taiki Kihara <recode.seadex777@gmail.com>
2019-06-12 16:46:13 +0900

    refactor: ディレクトリによって、bash_profileが呼べずにバグる問題の対応

--- setup_mac/setup_mojave.sh
+++ setup_mac/setup_mojave.sh
@@ -24,7 +24,7 @@ git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 cd ~/.nodenv && src/configure && make -C src
 echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(nodenv init -)"' >> ~/.bash_profile
-source .bash_profile
+source ~/.bash_profile
 git clone https://github.com/nodenv/node-build.git $(nodenv root)/plugins/node-build
 
 # install AppStore apps
